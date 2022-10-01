af53e4c3c2cbad34e054d0dc77f2038af41c63e1
sappymoon <vietjohnnydiep@gmail.com>
2022-10-01 18:14:52 -0500

    added decimal and backspace functionality

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ function getNumber(value){
     if(temp == 0){
         num1 += value;
         document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
+
+        if(value == '.'){
+            document.getElementById('decimal').value = '';
+        }
     }
     else if(temp == 1){
         num2 += value;
         document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
+
+        if(value == '.'){
+            document.getElementById('decimal').value = '';
+        }
     }
 }
 
@@ -22,36 +30,46 @@ function getOperator(value){
         operator = value;
         document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
         temp = 1;
+        document.getElementById('decimal').value = '.';
         lockOperator = 1;
     }
 }
 
 function operate(){
     if(operator == '/'){
-        answer = num1 / num2;
-        document.getElementById('display').innerHTML = answer;
-        lockOperator = 0;
-        num1 = answer;
+        if(num2 == 0){
+            document.getElementById('display').innerHTML = 'bruh';
+        }
+        else{
+            answer = num1 / num2;
+            document.getElementById('display').innerHTML = answer.toFixed(3) * 1;
+            lockOperator = 0;
+            num1 = answer;
+            document.getElementById('decimal').value = '.';
+        }
     }
     else if(operator == '*'){
         answer = num1 * num2;
-        document.getElementById('display').innerHTML = answer;
+        document.getElementById('display').innerHTML = answer.toFixed(3) * 1;
         lockOperator = 0;
         num1 = answer;
+        document.getElementById('decimal').value = '.';
     }
     else if(operator == '-'){
         answer = num1 - num2;
-        document.getElementById('display').innerHTML = answer;
+        document.getElementById('display').innerHTML = answer.toFixed(3) * 1;
         lockOperator = 0;
         num1 = answer;
+        document.getElementById('decimal').value = '.';
     }
     else if(operator == '+'){
         num1 = Number(num1);
         num2 = Number(num2);
         answer = num1 + num2;
-        document.getElementById('display').innerHTML = answer;
+        document.getElementById('display').innerHTML = answer.toFixed(3) * 1;
         lockOperator = 0;
         num1 = answer;
+        document.getElementById('decimal').value = '.';
     }
 }
 
@@ -62,4 +80,17 @@ function clearDisplay(){
     temp = 0;
 
     document.getElementById('display').innerHTML = '';
+}
+
+function deleteNumber(){
+    if(temp == 0){
+        num1 = num1.substring(0, num1.length - 1);
+        console.log(num1);
+        document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
+    }
+    else if(temp == 1){
+        num2 = num2.substring(0, num2.length - 1);
+        console.log(num2);
+        document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
+    }
 }
