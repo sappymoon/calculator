7288b6858a604fce533e16c5eb46fb1b0b7e5767
sappymoon <vietjohnnydiep@gmail.com>
2022-10-01 17:28:47 -0500

    made calculator fucntional

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let num1 = '';
 let num2 = '';
 let operator = '';
 let temp = 0;
+let answer = '';
+let lockOperator = 0;
 
 function getNumber(value){
     if(temp == 0){
@@ -15,7 +17,49 @@ function getNumber(value){
 }
 
 function getOperator(value){
-    operator = value;
-    document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
-    temp = 1;
+    if(lockOperator == 0){
+        num2 = '';
+        operator = value;
+        document.getElementById('display').innerHTML = num1 + " " + operator + " " + num2;
+        temp = 1;
+        lockOperator = 1;
+    }
+}
+
+function operate(){
+    if(operator == '/'){
+        answer = num1 / num2;
+        document.getElementById('display').innerHTML = answer;
+        lockOperator = 0;
+        num1 = answer;
+    }
+    else if(operator == '*'){
+        answer = num1 * num2;
+        document.getElementById('display').innerHTML = answer;
+        lockOperator = 0;
+        num1 = answer;
+    }
+    else if(operator == '-'){
+        answer = num1 - num2;
+        document.getElementById('display').innerHTML = answer;
+        lockOperator = 0;
+        num1 = answer;
+    }
+    else if(operator == '+'){
+        num1 = Number(num1);
+        num2 = Number(num2);
+        answer = num1 + num2;
+        document.getElementById('display').innerHTML = answer;
+        lockOperator = 0;
+        num1 = answer;
+    }
+}
+
+function clearDisplay(){
+    num1 = '';
+    num2 = '';
+    operator = '';
+    temp = 0;
+
+    document.getElementById('display').innerHTML = '';
 }
